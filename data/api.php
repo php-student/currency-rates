c68c020206c1d89b2c7a3056f4c62f71b8f3eed9
user <danilglv-a@mail.ru>
2015-10-18 00:02:24 +0600

    Экзаменационное

---
+++ data/api.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 16.10.2015
+ * Time: 20:21
+ */
+function currencyExchange($CurrentValue, $curr){
+	if($curr !=$CurrentValue){
+		$api = 'http://api.fixer.io/latest?base=';
+		$json = file_get_contents($api . $CurrentValue);
+		$data = json_decode($json, 1);
+		return round($data['rates'][$curr], 2);
+	}
+	else return 1;
+}
---
+++ data/dbCurrency.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 15.10.2015
+ * Time: 23:51
+ */
+return array(
+  'glyphicon glyphicon-usd' => 'USD',
+  'glyphicon glyphicon-eur' => 'EUR',
+  'glyphicon glyphicon-rub' => 'RUB',
+);
